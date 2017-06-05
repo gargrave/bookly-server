5762ae68321e90f02a0486219b3cdc8f907462cf
Gabe Hargrave <gkhargrave@gmail.com>
2017-06-05 09:38:58 -0700

    Restrict DB seeding process to 'dev' environment

--- scripts/db/seed.js
+++ scripts/db/seed.js
@@ -4,13 +4,20 @@ const config = {
   directory: './database/seeds/'
 }
 
-knex.seed.run(config)
-  .then(res => {
-    console.log('DB seeding successful!')
-    process.exit(0)
-  })
-  .catch(err => {
-    console.log('DB seeding error!')
-    console.log(err)
-    process.exit(1)
-  })
+// ensure that we are in 'dev' environment; otherwise, do not seed!
+if (process.env.NODE_ENV !== 'dev') {
+  console.log('Database seeding can only be run in "dev" environment.')
+  console.log('Nothing will be done.')
+  process.exit(0)
+} else {
+  knex.seed.run(config)
+    .then(res => {
+      console.log('DB seeding successful!')
+      process.exit(0)
+    })
+    .catch(err => {
+      console.log('DB seeding error!')
+      console.log(err)
+      process.exit(1)
+    })
+}
