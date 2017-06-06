8c52d6696725fb941b7f5478a3051379c4930e57
Gabe Hargrave <gkhargrave@gmail.com>
2017-06-06 16:40:24 -0700

    Update mock/seed data to be a bit more useful

--- database/mocks/booksMock.js
+++ database/mocks/booksMock.js
@@ -2,26 +2,23 @@
 
 let data = [
   { ownerId: 1, authorId: 1, title: 'Mother Night' },
-  { ownerId: 1, authorId: 3, title: 'Even Cowgirls Get the Blues' },
-  { ownerId: 1, authorId: 6, title: 'Nine Stories' },
   { ownerId: 1, authorId: 1, title: 'Slapstick' },
-  { ownerId: 1, authorId: 5, title: 'The Doors of Perception' },
-  { ownerId: 1, authorId: 4, title: 'A Short History of Nearly Everything' },
-  { ownerId: 1, authorId: 2, title: 'Fight Club' },
   { ownerId: 1, authorId: 1, title: 'Slaughterhouse Five' },
-  { ownerId: 1, authorId: 7, title: 'The Metamorphasis' },
+  { ownerId: 1, authorId: 1, title: 'The Sirens of Titan' },
+  { ownerId: 1, authorId: 2, title: 'Fight Club' },
+  { ownerId: 1, authorId: 2, title: 'Choke' },
+  { ownerId: 1, authorId: 2, title: 'Survivor' },
+  { ownerId: 1, authorId: 3, title: 'Even Cowgirls Get the Blues' },
   { ownerId: 1, authorId: 3, title: 'Jitterbug Perfume' },
+  { ownerId: 1, authorId: 3, title: 'Half Asleep in Frog Pajamas' },
 
-  { ownerId: 2, authorId: 9, title: 'Choke' },
-  { ownerId: 2, authorId: 11, title: 'The Life and Times of the Thunderbolt Kid' },
-  { ownerId: 2, authorId: 12, title: 'Island' },
-  { ownerId: 2, authorId: 8, title: 'The Sirens of Titan' },
-  { ownerId: 2, authorId: 12, title: 'Half Asleep in Frog Pajamas' },
-  { ownerId: 2, authorId: 9, title: 'Survivor' },
-  { ownerId: 2, authorId: 14, title: 'The Trial' },
-  { ownerId: 2, authorId: 11, title: 'A Walk in the Woods' },
-  { ownerId: 2, authorId: 12, title: 'Brave New World' },
-  { ownerId: 2, authorId: 13, title: 'Cather in the Rye' }
+  { ownerId: 2, authorId: 4, title: 'A Walk in the Woods' },
+  { ownerId: 2, authorId: 4, title: 'The Life and Times of the Thunderbolt Kid' },
+  { ownerId: 2, authorId: 5, title: 'Island' },
+  { ownerId: 2, authorId: 5, title: 'Brave New World' },
+  { ownerId: 2, authorId: 6, title: 'Cather in the Rye' },
+  { ownerId: 2, authorId: 7, title: 'The Trial' },
+  { ownerId: 2, authorId: 7, title: 'The Metamorphasis' }
 ]
 
 module.exports = {
