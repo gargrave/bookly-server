795949cfa1f6dd7f7aad9e6e138ad4fe0bc7825b
Gabe Hargrave <gkhargrave@gmail.com>
2017-06-06 16:42:39 -0700

    Remove unnecessary route prerequisites

--- api/books/utils/bookPrereqs.js
+++ api/books/utils/bookPrereqs.js
@@ -1,25 +1,4 @@
 'use strict'
 
-const Boom = require('boom')
-
-const knex = require('../../../database/db')
-const DB = require('../../../globals/constants').db
-const apiErr = require('../../utils/apiErrors')
-
-const modelName = 'Book'
-const dbName = DB.BOOKS
-
 module.exports = {
-  ensureUnique (request, reply) {
-    const { authorId, title } = request.payload
-
-    knex(dbName)
-      .where({ authorId, title })
-        .then(result => {
-          if (result.length) {
-            return reply(Boom.badRequest(apiErr.matchingRecord(modelName)))
-          }
-          return reply()
-        })
-  }
 }
