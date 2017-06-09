b6634de36ceeebfabb37229fbb358e82cbe09d01
Gabe Hargrave <gkhargrave@gmail.com>
2017-06-09 15:12:45 -0700

    Refactor: move user detail route to class-based route

--- api/auth/routes/userDetail.js
+++ api/auth/routes/userDetail.js
@@ -1,40 +1,52 @@
 'use strict'
 
-const Boom = require('boom')
+const ApiRoute = require('../../generic-routes/basic').ApiRoute
 
-const APIRoute = require('../../generic-routes/basic').APIRoute
+const Boom = require('boom')
 
 const DB = require('../../../globals/constants').db
 const knex = require('../../../database/db')
 const apiErr = require('../../utils/apiErrors')
 
-function UserDetailRoute () {
-  APIRoute.call(this, 'GET', 'auth/users')
+const params = {
+  method: 'GET',
+  path: 'auth/users',
+  db: DB.USERS,
+  resourceName: 'User'
+}
 
-  this.config.handler = (request, reply) => {
-    const ownerId = request.auth.credentials.id
-    if (!ownerId || !Number.isInteger(ownerId)) {
-      reply(Boom.unauthorized())
+class UserDetailRoute extends ApiRoute {
+  constructor () {
+    super(params)
+    this.db = params.db
+    this.resourceName = params.resourceName
+  }
+
+  getHandler () {
+    return (request, reply) => {
+      const ownerId = request.auth.credentials.id
+      if (!ownerId || !Number.isInteger(ownerId)) {
+        reply(Boom.unauthorized())
+      }
+
+      const id = request.params.id
+
+      knex(this.db)
+        .select(this.getSelectParams())
+        .where({ 'id': ownerId })
+        .limit(1)
+        .then(result => {
+          if (!result.length) {
+            return reply(Boom.notFound(apiErr.notFound(this.resourceName, id)))
+          }
+          reply(result[0])
+        })
     }
+  }
 
-    const id = request.params.id
-
-    knex(DB.USERS)
-      .select(this.getSelectCols())
-      .where({ 'id': ownerId })
-      .limit(1)
-      .then(result => {
-        if (!result.length) {
-          return reply(Boom.notFound(apiErr.notFound('User', id)))
-        }
-        reply(result[0])
-      })
+  getSelectParams () {
+    return ['id', 'email', 'created_at', 'updated_at']
   }
 }
-UserDetailRoute.prototype = Object.create(APIRoute.prototype)
 
-UserDetailRoute.prototype.getSelectCols = function () {
-  return ['id', 'email', 'created_at', 'updated_at']
-}
-
-module.exports = new UserDetailRoute()
+module.exports = new UserDetailRoute().buildRoute()
