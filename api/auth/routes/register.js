d3cdd8f1ea781f836db77d2cd675c99f3b5c62c7
Gabe Hargrave <gkhargrave@gmail.com>
2017-06-09 15:00:15 -0700

    Refactor: move 'register' route to class-based route

--- api/auth/routes/register.js
+++ api/auth/routes/register.js
@@ -1,53 +1,57 @@
 'use strict'
 
-const Bcrypt = require('bcrypt')
+const ApiCreateRoute = require('../../generic-routes/create').ApiCreateRoute
 
-const APICreateRoute = require('../../generic-routes/create').APICreateRoute
+const Bcrypt = require('bcrypt')
 
 const DB = require('../../../globals/constants').db
 const validator = require('../utils/authValidator')
 
-const SALT_ROUNDS = 10
+const params = {
+  path: 'auth/register',
+  db: DB.USERS,
+  resourceName: 'Users',
+  auth: false
+}
 
-function RegisterRoute () {
-  const params = {
-    path: 'auth/register',
-    db: DB.USERS,
-    resourceName: 'Users',
-    auth: false
+class RegisterRoute extends ApiCreateRoute {
+  constructor () {
+    super(params)
   }
-  APICreateRoute.call(this, params)
-}
-RegisterRoute.prototype = Object.create(APICreateRoute.prototype)
 
-/*
-Override to do the following:
-  - Remove password from the selected cols; even though it is a hash at this point,
-      it is still preferable not to return it.
-*/
-RegisterRoute.prototype.getSelectCols = function () {
-  return ['id', 'email', 'created_at', 'updated_at']
-}
+  /**
+   * Override to do the following:
+   *    - Remove password from the selected cols;
+   *      even though it is a hash at this point, it is still preferable not to return it.
+   */
+  getSelectParams () {
+    return ['id', 'email', 'created_at', 'updated_at']
+  }
 
-/*
-Override to do the following:
-  - Replace original password with hashed version before it goes to DB
-  - Remove the 'passwordConfirm' property, as it is no longer needed at this point
-*/
-RegisterRoute.prototype.buildPayload = function (payload) {
-  return new Promise((resolve, reject) => {
-    Bcrypt.hash(payload.password, SALT_ROUNDS, (err, hash) => {
-      if (err) {
-        console.log(`Error in generating hash: ${err}`)
-        reject(payload)
-      }
+  /**
+   * Override to do the following:
+   *    - Replace original password with hashed version before it goes to DB
+   *    - Remove the 'passwordConfirm' property, as it is no longer needed at this point
+   */
+  buildPayload (payload) {
+    const SALT_ROUNDS = 10
+    return new Promise((resolve, reject) => {
+      Bcrypt.hash(payload.password, SALT_ROUNDS, (err, hash) => {
+        if (err) {
+          console.error(`Error in generating hash: ${err}`)
+          reject(payload)
+        }
 
-      payload.password = hash
-      delete payload.passwordConfirm
-      resolve(payload)
+        payload.password = hash
+        delete payload.passwordConfirm
+        resolve(payload)
+      })
     })
-  })
+  }
+
+  getValidators () {
+    return { payload: validator.onRegister }
+  }
 }
 
-module.exports = new RegisterRoute()
-  .validate({ payload: validator.onRegister })
+module.exports = new RegisterRoute().buildRoute()
