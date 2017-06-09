949b315fd0bef530b7324c10d6fe75b7db2a806f
Gabe Hargrave <gkhargrave@gmail.com>
2017-06-09 15:12:30 -0700

    Refactor: move login route to class-based route

--- api/auth/routes/login.js
+++ api/auth/routes/login.js
@@ -1,61 +1,74 @@
 'use strict'
 
+const ApiRoute = require('../../generic-routes/basic').ApiRoute
+
 const Bcrypt = require('bcrypt')
 const Boom = require('boom')
 const JWT = require('jsonwebtoken')
-
-const APIRoute = require('../../generic-routes/basic').APIRoute
 
 const knex = require('../../../database/db')
 const DB = require('../../../globals/constants').db
 const apiErr = require('../../utils/apiErrors')
 const validator = require('../utils/authValidator')
 
-const db = DB.USERS
-const resourceName = 'User'
+const params = {
+  method: 'POST',
+  path: 'auth/login',
+  auth: false,
+  db: DB.USERS,
+  resourceName: 'User'
+}
 
-function LoginRoute () {
-  APIRoute.call(this, 'POST', 'auth/login', false)
+class LoginRoute extends ApiRoute {
+  constructor () {
+    super(params)
+    this.db = params.db
+    this.resourceName = params.resourceName
+  }
 
-  this.config.handler = (request, reply) => {
-    const email = request.payload.email.trim()
+  getHandler () {
+    return (request, reply) => {
+      const email = request.payload.email.trim()
 
-    knex(db)
-      .where('email', email)
-      .limit(1)
-      .then(result => {
-        if (!result.length) {
-          return reply(Boom.notFound(apiErr.notFound(resourceName, email)))
-        }
-
-        const user = result[0]
-        const hashedPassword = user.password
-        const submittedPassword = request.payload.password
-
-        Bcrypt.compare(submittedPassword, hashedPassword, function (err, match) {
-          if (err) {
-            // some unrelated error
-            console.log(err)
-            return reply(Boom.notFound(apiErr.notFound(resourceName, email)))
-          } else if (!match) {
-            // non-matching username/password
-            return reply(Boom.badRequest(apiErr.invalidLogin()))
+      knex(this.db)
+        .where('email', email)
+        .limit(1)
+        .then(result => {
+          if (!result.length) {
+            return reply(Boom.notFound(apiErr.notFound(this.resourceName, email)))
           }
 
-          // generate a JWT, and add it to reply
-          const jwtData = { id: user.id, email: user.email }
-          const jwtOptions = { expiresIn: 60 * 5 }
-          const token = JWT.sign(jwtData, process.env.AUTH_SECRET_KEY, jwtOptions)
-          user.token = token
+          const user = result[0]
+          const hashedPassword = user.password
+          const submittedPassword = request.payload.password
 
-          // remove the password hash from the reply
-          delete user.password
-          return reply(user)
+          Bcrypt.compare(submittedPassword, hashedPassword, function (err, match) {
+            if (err) {
+              // some unrelated error
+              console.log(err)
+              return reply(Boom.notFound(apiErr.notFound(this.esourceName, email)))
+            } else if (!match) {
+              // non-matching username/password
+              return reply(Boom.badRequest(apiErr.invalidLogin()))
+            }
+
+            // generate a JWT, and add it to reply
+            const jwtData = { id: user.id, email: user.email }
+            const jwtOptions = { expiresIn: 60 * 60 }
+            const token = JWT.sign(jwtData, process.env.AUTH_SECRET_KEY, jwtOptions)
+            user.token = token
+
+            // remove the password hash from the reply
+            delete user.password
+            return reply(user)
+          })
         })
-      })
+    }
+  }
+
+  getValidators () {
+    return { payload: validator.onLogin }
   }
 }
-LoginRoute.prototype = Object.create(APIRoute.prototype)
 
-module.exports = new LoginRoute()
-  .validate({ payload: validator.onLogin })
+module.exports = new LoginRoute().buildRoute()
