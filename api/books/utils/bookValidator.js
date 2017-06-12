1bc073ce282b97ead0b471c8c7150cf5e41922f3
Gabe Hargrave <gkhargrave@gmail.com>
2017-06-12 14:10:06 -0700

    Add 'id' as optional prop to Book validation

--- api/books/utils/bookValidator.js
+++ api/books/utils/bookValidator.js
@@ -3,6 +3,7 @@
 const Joi = require('joi')
 
 const baseValidator = {
+  id: Joi.number().integer(),
   authorId: Joi.number().integer().min(1).required(),
   title: Joi.string().max(255).required()
 }
