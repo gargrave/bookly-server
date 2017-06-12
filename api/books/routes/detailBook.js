645f61e9ae91b23703faffdd8ba8c96ad7ebfb02
Gabe Hargrave <gkhargrave@gmail.com>
2017-06-12 14:11:36 -0700

    Update Book detail route to include Author data in response

--- api/books/routes/detailBook.js
+++ api/books/routes/detailBook.js
@@ -1,7 +1,13 @@
 'use strict'
 
 const ApiDetailRoute = require('../../generic-routes/detail')
+
+const Boom = require('boom')
+
 const DB = require('../../../globals/constants').db
+const apiErr = require('../../utils/apiErrors')
+const knex = require('../../../database/db')
+const helpers = require('../utils/bookRouteHelpers')
 
 const params = {
   path: 'books',
@@ -14,8 +20,37 @@ class BookDetailRoute extends ApiDetailRoute {
     super(params)
   }
 
+  /*
+   * Override of the handler builder to run a JOIN command on the Author data.
+   */
+  getHandler () {
+    return (request, reply) => {
+      const ownerId = request.auth.credentials.id
+      if (!ownerId || !Number.isInteger(ownerId)) {
+        reply(Boom.unauthorized())
+      }
+
+      const id = request.params.id
+
+      knex(this.db)
+        .select(this.getSelectParams())
+        .innerJoin(DB.AUTHORS, `${DB.BOOKS}.authorId`, `${DB.AUTHORS}.id`)
+        .where({
+          [`${DB.BOOKS}.ownerId`]: ownerId,
+          [`${DB.BOOKS}.id`]: request.params.id
+        })
+        .limit(1)
+        .then(result => {
+          if (!result.length) {
+            return reply(Boom.notFound(apiErr.notFound(this.resourceName, id)))
+          }
+          reply(helpers.populateAuthor(result[0]))
+        })
+    }
+  }
+
   getSelectParams () {
-    return ['id', 'authorId', 'title', 'created_at', 'updated_at']
+    return helpers.selectCols
   }
 }
 
