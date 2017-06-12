429db8aba85635bafc8c374c177fe7771937a98f
Gabe Hargrave <gkhargrave@gmail.com>
2017-06-12 13:43:33 -0700

    Update Books.List route to provide a cleaner response
    - Run a JOIN query on the SELECT query to get author data
    - Parse the author data into a cleaner format for the response

--- api/books/routes/listBooks.js
+++ api/books/routes/listBooks.js
@@ -1,7 +1,11 @@
 'use strict'
 
 const ApiListRoute = require('../../generic-routes/list')
+
+const Boom = require('boom')
+
 const DB = require('../../../globals/constants').db
+const knex = require('../../../database/db')
 
 class BooksListRoute extends ApiListRoute {
   constructor () {
@@ -11,8 +15,50 @@ class BooksListRoute extends ApiListRoute {
     })
   }
 
+  /*
+   * Override of the handler builder to run a JOIN command on the Author data.
+   */
+  getHandler () {
+    return (request, reply) => {
+      const ownerId = request.auth.credentials.id
+      if (!ownerId || !Number.isInteger(ownerId)) {
+        reply(Boom.unauthorized())
+      }
+
+      knex(this.db)
+        .select(this.getSelectParams())
+        .innerJoin(DB.AUTHORS, `${DB.BOOKS}.authorId`, `${DB.AUTHORS}.id`)
+        .where({ [`${DB.BOOKS}.ownerId`]: ownerId })
+          .then(results => {
+            reply(this.parseReply(results))
+          })
+    }
+  }
+
   getSelectParams () {
-    return ['id', 'authorId', 'title', 'created_at', 'updated_at']
+    return [
+      'Books.id', 'title', 'Books.created_at', 'Books.updated_at',
+      'authorId', 'Authors.firstName', 'Authors.lastName']
+  }
+
+  /*
+   * Custom reply parser to build a nice clean 'author' object within the book.
+   * The original DB query has all the data, but it is not very presented.
+   */
+  parseReply (reply) {
+    for (let book of reply) {
+      // build a cleaner version of the author
+      book.author = {
+        id: book.authorId,
+        name: `${book.firstName} ${book.lastName}`
+      }
+
+      // remove the original fields
+      delete book.authorId
+      delete book.firstName
+      delete book.lastName
+    }
+    return reply
   }
 }
 
