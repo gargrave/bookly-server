46d47097931b6739affbabbd0126c5429d6ab8e9
Gabe Hargrave <gkhargrave@gmail.com>
2017-06-12 15:20:43 -0700

    Update create/update routes for cleaner Author data parsing

--- api/books/routes/createBook.js
+++ api/books/routes/createBook.js
@@ -2,8 +2,13 @@
 
 const ApiCreateRoute = require('../../generic-routes/create')
 
+const Boom = require('boom')
+
+const knex = require('../../../database/db')
 const DB = require('../../../globals/constants').db
+const apiErr = require('../../utils/apiErrors')
 const prereqs = require('../../utils/prereqs')
+const helpers = require('../utils/bookRouteHelpers')
 const validator = require('../utils/bookValidator')
 
 const params = {
@@ -17,8 +22,50 @@ class BookCreateRoute extends ApiCreateRoute {
     super(params)
   }
 
+  getHandler () {
+    return (request, reply) => {
+      this.buildPayload(request.payload)
+        .then(data => {
+          knex(this.db)
+            .insert(data)
+            .returning(this.getSelectParams())
+              .then(result => {
+                // HACK
+                // this is a hack to rebuild the response with full author data,
+                // as I could not initially figure out how to get Knex to do a
+                // JOIN and RETURNING clause at the same time
+                const authorId = data.authorId
+                knex(DB.AUTHORS)
+                  .select(['firstName', 'lastName', 'id'])
+                  .where({
+                    [`${DB.AUTHORS}.ownerId`]: data.ownerId,
+                    [`${DB.AUTHORS}.id`]: authorId
+                  })
+                  .limit(1)
+                  .then(authorResult => {
+                    if (!authorResult.length) {
+                      return reply(Boom.notFound(apiErr.notFound('Author', authorId)))
+                    }
+                    reply(helpers.populateAuthor(
+                      Object.assign({}, result[0], authorResult[0]))
+                    )
+                  })
+                  // ENDHACK
+              }, err => {
+                console.error(err)
+                reply(Boom.badRequest(apiErr.failedToCreate(this.resourceName)))
+              })
+        }, err => {
+          console.error(err)
+          reply(Boom.badRequest(apiErr.failedToCreate(this.resourceName)))
+        })
+    }
+  }
+
   getSelectParams () {
-    return ['id', 'authorId', 'title', 'created_at', 'updated_at']
+    return [
+      'id', 'title', 'created_at', 'updated_at', 'authorId'
+    ]
   }
 
   getPrerequisites () {
--- api/books/routes/updateBook.js
+++ api/books/routes/updateBook.js
@@ -2,7 +2,12 @@
 
 const ApiUpdateRoute = require('../../generic-routes/update')
 
+const Boom = require('boom')
+
+const knex = require('../../../database/db')
 const DB = require('../../../globals/constants').db
+const apiErr = require('../../utils/apiErrors')
+const helpers = require('../utils/bookRouteHelpers')
 const validator = require('../utils/bookValidator')
 
 const params = {
@@ -14,6 +19,54 @@ const params = {
 class BookUpdateRoute extends ApiUpdateRoute {
   constructor () {
     super(params)
+  }
+
+  getHandler () {
+    return (request, reply) => {
+      const ownerId = request.auth.credentials.id
+      if (!ownerId || !Number.isInteger(ownerId)) {
+        reply(Boom.unauthorized())
+      }
+
+      const id = request.params.id
+
+      this.buildPayload(request.payload)
+        .then(data => {
+          knex(this.db)
+            .where({ id, ownerId })
+            .update(data)
+            .returning(this.getSelectParams())
+              .then(result => {
+                // HACK
+                // this is a hack to rebuild the response with full author data,
+                // as I could not initially figure out how to get Knex to do a
+                // JOIN and RETURNING clause at the same time
+                const authorId = result[0].authorId
+                knex(DB.AUTHORS)
+                  .select(['firstName', 'lastName', 'id'])
+                  .where({
+                    ownerId,
+                    id: authorId
+                  })
+                  .limit(1)
+                  .then(authorResult => {
+                    if (!authorResult.length) {
+                      return reply(Boom.notFound(apiErr.notFound('Author', authorId)))
+                    }
+                    reply(helpers.populateAuthor(
+                      Object.assign({}, result[0], authorResult[0]))
+                    )
+                  })
+                  // ENDHACK
+              }, err => {
+                console.log(err)
+                reply(Boom.badRequest(apiErr.failedToUpdate(this.resourceName)))
+              })
+        }, err => {
+          console.log(err)
+          reply(Boom.badRequest(apiErr.failedToUpdate(this.resourceName)))
+        })
+    }
   }
 
   getSelectParams () {
