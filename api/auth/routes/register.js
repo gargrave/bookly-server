ec8fcd4937c98f95e77d04b5dff0dd4d135c416c
Gabe Hargrave <gkhargrave@gmail.com>
2017-06-23 20:40:04 -0700

    Refactor: update Register route to use async/await pattern

--- api/auth/routes/register.js
+++ api/auth/routes/register.js
@@ -4,12 +4,12 @@ const ApiCreateRoute = require('../../generic-routes/create')
 
 const Bcrypt = require('bcrypt-nodejs')
 const Boom = require('boom')
-const JWT = require('jsonwebtoken')
 
 const knex = require('../../../database/db')
 const DB = require('../../../globals/constants').db
 const env = require('../../../globals/env')
 const apiErr = require('../../utils/apiErrors')
+const helpers = require('../utils/authRouteHelpers')
 const validator = require('../utils/authValidator')
 
 const params = {
@@ -26,42 +26,15 @@ class RegisterRoute extends ApiCreateRoute {
 
   getHandler () {
     return (request, reply) => {
-      this.buildPayload(request.payload)
-        .then(data => {
-          knex(this.db)
-            .insert(data)
-            .returning(this.getSelectParams())
-              .then(result => {
-                const user = result[0]
-                // TODO: move this to an 'after hook'
-                // generate a JWT, and add it to reply
-                const jwtData = { id: user.id, email: user.email }
-                const jwtOptions = { expiresIn: 60 * 60 * 8 }
-                const token = JWT.sign(jwtData, process.env.AUTH_SECRET_KEY, jwtOptions)
-                user.token = token
-                reply(user)
-              }, err => {
-                if (env.isDevEnv()) {
-                  console.error(err)
-                }
-                reply(Boom.badRequest(apiErr.userExists()))
-              })
-        }, err => {
-          if (env.isDevEnv()) {
-            console.error(err)
-          }
-          reply(Boom.badRequest(apiErr.failedToCreate(this.resourceName)))
-        })
+      this.buildPayload(request.payload).then(data => {
+        this.query(data).then(res => reply(res))
+      }, err => {
+        if (env.isDevEnv()) {
+          console.error(err)
+        }
+        reply(Boom.badRequest(apiErr.failedToCreate(this.resourceName)))
+      })
     }
-  }
-
-  /**
-   * Override to do the following:
-   *    - Remove password from the selected cols;
-   *      even though it is a hash at this point, it is still preferable not to return it.
-   */
-  getSelectParams () {
-    return ['id', 'email', 'created_at', 'updated_at']
   }
 
   /**
@@ -70,11 +43,12 @@ class RegisterRoute extends ApiCreateRoute {
    *    - Remove the 'passwordConfirm' property, as it is no longer needed at this point
    */
   buildPayload (payload) {
-    // const SALT_ROUNDS = 10
     return new Promise((resolve, reject) => {
       Bcrypt.hash(payload.password, null, null, (err, hash) => {
         if (err) {
-          console.error(`Error in generating hash: ${err}`)
+          if (env.isDevEnv()) {
+            console.error(`Error in generating hash: ${err}`)
+          }
           reject(payload)
         }
 
@@ -83,6 +57,35 @@ class RegisterRoute extends ApiCreateRoute {
         resolve(payload)
       })
     })
+  }
+
+  async query (data) {
+    let result = await this.runInsertQuery(data)
+    return result
+  }
+
+  async runInsertQuery (data) {
+    let val = Boom.badRequest(apiErr.userExists())
+
+    await knex(this.db)
+      .insert(data)
+      .returning(this.getSelectParams())
+      .then(res => {
+        // generate a JWT, and add it to reply
+        const user = res[0]
+        user.token = helpers.buildJWT(user)
+        val = user
+      }, err => {
+        if (env.isDevEnv()) {
+          console.error(err)
+        }
+      })
+
+    return val
+  }
+
+  getSelectParams () {
+    return helpers.selectCols
   }
 
   getValidators () {
