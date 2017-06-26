37c2fe0f9b984f8d0abd74e77b484f304c5db193
Gabe Hargrave <gkhargrave@gmail.com>
2017-06-26 14:52:25 -0700

    Finish implementing verify-account route

--- api/auth/routes/verify-account.js
+++ api/auth/routes/verify-account.js
@@ -6,9 +6,7 @@ const Boom = require('boom')
 
 const DB = require('../../../globals/constants').db
 const knex = require('../../../database/db')
-const apiErr = require('../../utils/apiErrors')
-const globalHelpers = require('../../utils/routeHelpers')
-const helpers = require('../utils/authRouteHelpers')
+const env = require('../../../globals/env')
 
 const params = {
   method: 'POST',
@@ -35,12 +33,14 @@ class VerifyAccountRoute extends ApiRoute {
     // if so, use that email to update the matching account's verified to TRUE
     // if not, return an error message
     let result = await this.findExistingToken(token)
-    result = await this.markAccountAsVerified(result)
+    if (result.token) {
+      result = { verified: await this.markAccountAsVerified(result) }
+    }
     return result
   }
 
   async findExistingToken (token) {
-    let val
+    let val = Boom.notFound()
 
     await knex(DB.TOKENS_VERIFY)
       .select()
@@ -55,13 +55,36 @@ class VerifyAccountRoute extends ApiRoute {
     return val
   }
 
-  async markAccountAsVerified (token) {
-    console.log('*****************************************')
-    console.log('* TODO: verify-account.js')
-    console.log('*   - Run an UPDATE query on the User\'s "verified" state')
-    console.log('*   - Delete the entry from the tokens table, as it is no longer needed')
-    console.log('*****************************************')
-    return token
+  async markAccountAsVerified (tokenData) {
+    let verified = false
+    let where = { email: tokenData.email }
+    const data = { verified: true }
+
+    // update the Users DB to show this user as verified
+    await knex(DB.USERS)
+      .where(where)
+      .update(data)
+      .then(res => {
+        if (res) {
+          verified = true
+        }
+      })
+
+    // then delete the token, as we no longer need it
+    where.token = tokenData.token
+    await knex(DB.TOKENS_VERIFY)
+      .where(where)
+      .del()
+      .then(() => {
+        // if the query is successful, we don't need to do anything here
+      }, err => {
+        if (env.isDevEnv()) {
+          console.log('Error!')
+          console.error(err)
+        }
+      })
+
+    return verified
   }
 }
 
