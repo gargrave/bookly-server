41f3672f17df542c98246cc25ab8e81f53a6f36e
Gabe Hargrave <gkhargrave@gmail.com>
2017-06-28 11:56:29 -0700

    Add validator for "reset password" route

--- api/auth/utils/authValidator.js
+++ api/auth/utils/authValidator.js
@@ -24,6 +24,41 @@ const baseValidator = {
     })
 }
 
+const emailVal = {
+  // email must be valid
+  email: Joi.string()
+    .trim()
+    .email()
+    .required()
+}
+
+const passwordVal = {
+  password: Joi.string()
+    .trim()
+    .regex(/^[\w\d!@#$%^&*_]+$/)
+    .min(8).max(72)
+    .required()
+    .options({
+      language: {
+        string: {
+          regex: { base: 'Password contains illegal characters.' }
+        }
+      }
+    })
+}
+
+const passwordConfirmVal = {
+  // password confirm must match password
+  passwordConfirm: Joi.any()
+    .valid(Joi.ref('password'))
+    .required()
+    .options({
+      language: {
+        any: { allowOnly: 'Passwords do not match.' }
+      }
+    })
+}
+
 module.exports = {
   onRegister: Joi.object(Object.assign({},
     baseValidator, {
@@ -38,6 +73,12 @@ module.exports = {
         })
     })
   ),
+
+  passwordReset: Joi.object(Object.assign({},
+    { token: Joi.string().required() },
+    passwordVal,
+    passwordConfirmVal
+  )),
 
   onLogin: Joi.object(baseValidator)
 }
