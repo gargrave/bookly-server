e6177ef8210b6de53c8f3bb7a6972b0fb730c88d
Gabe Hargrave <gkhargrave@gmail.com>
2017-07-03 14:46:09 -0700

    Add nice helpful API error parsing function

--- api/utils/utils.js
+++ api/utils/utils.js
@@ -1,5 +1,14 @@
 module.exports = {
   getRandomToken (len = 48) {
     return require('crypto').randomBytes(len).toString('hex')
+  },
+
+  parseError (err) {
+    if (err.response && err.response.data) {
+      return err.response.data
+    } else if (err.message) {
+      return err
+    }
+    return { message: 'An unknown error occurred.' }
   }
 }
