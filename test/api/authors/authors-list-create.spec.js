09cc8d98cb412033e3640b61a0378a3e16a077f8
Gabe Hargrave <gkhargrave@gmail.com>
2017-07-04 10:15:39 -0700

    Add tests for Authors.List route

---
+++ test/api/authors/authors-list-create.spec.js
@@ -0,0 +1,72 @@
+const apiHelper = require('../../../api/utils/api-helper')
+
+const testHelper = require('../../test-helper')
+const { login, testHttp } = testHelper
+
+const authorsUrl = 'http://localhost:3001/api/v1/authors'
+
+describe('API ROUTE: /authors/ (GET: list) -> ', () => {
+  let response
+
+  describe('Not Authenticated ->', () => {
+    before((done) => {
+      let request = apiHelper.axGet(authorsUrl)
+      // send the HTTP request
+      testHttp(request).then(res => {
+        response = res.data
+        done()
+      })
+    })
+
+    it('should return a 401 error if no token is provided', (done) => {
+      expect(response.statusCode).to.equal(401)
+      expect(response.message).to.not.be.undefined()
+      done()
+    })
+  })
+
+  describe('Not Authenticated ->', () => {
+    before((done) => {
+      let request = apiHelper.axGet(authorsUrl)
+      // send the HTTP request
+      testHttp(request).then(res => {
+        response = res.data
+        done()
+      })
+    })
+
+    it('should return a 401 error if an invalid token is provided', (done) => {
+      expect(response.statusCode).to.equal(401)
+      expect(response.message).to.not.be.undefined()
+      done()
+    })
+  })
+
+  describe('Authenticated ->', () => {
+    before((done) => {
+      login().then(token => {
+        let request = apiHelper.axGet(authorsUrl, token)
+        // send the HTTP request
+        testHttp(request).then(res => {
+          response = res.data
+          done()
+        })
+      })
+    })
+
+    it('should a list of Authors if proper authentication is provided', (done) => {
+      let authors = response
+      let author = authors[0]
+
+      expect(authors).to.be.an.array()
+      expect(authors.length > 0).to.be.true()
+      expect(author.id).to.not.be.undefined()
+      expect(author.firstName).to.not.be.undefined()
+      expect(author.lastName).to.not.be.undefined()
+      expect(author.created_at).to.not.be.undefined()
+      expect(author.updated_at).to.not.be.undefined()
+
+      done()
+    })
+  })
+})
