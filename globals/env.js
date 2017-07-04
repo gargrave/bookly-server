88eae9b68ddcdba923bce392ceb83523040ad65d
Gabe Hargrave <gkhargrave@gmail.com>
2017-07-04 11:21:17 -0700

    Add wrapper function for conditionally displaying error messages

--- globals/env.js
+++ globals/env.js
@@ -1,5 +1,16 @@
+const printFullErrors = (process.env.SHOW_FULL_ERRORS_IN_DEV === 'true')
+
 module.exports = {
   isProdEnv: () => process.env.NODE_ENV === 'prod',
   isStagingEnv: () => process.env.NODE_ENV === 'staging',
-  isDevEnv: () => process.env.NODE_ENV === 'dev'
+  isDevEnv: () => process.env.NODE_ENV === 'dev',
+
+  error (err, location) {
+    if (this.isDevEnv()) {
+      console.log(`Error @ ${location}`)
+      if (printFullErrors) {
+        console.error(err)
+      }
+    }
+  }
 }
